6c91d988362b04da97b785f94fff134958bdaf97
ivansevern <ivansevern@gmail.com>
2022-05-17 17:52:39 +0300

    add

--- src/main/java/jdbc/CableDb.java
+++ src/main/java/jdbc/CableDb.java
@@ -1,7 +1,12 @@
 package jdbc;
 
+import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Properties;
 
 @UtilityClass
@@ -12,7 +17,15 @@ public class CableDb {
     static {
         try {
             DB_Settings.load(DBRunner.class.getResourceAsStream("/db.properties"));
-
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    @SneakyThrows(SQLException.class)
+    public void dropBooksAndAuthorsIfExists() {
+        try (final Connection connection = getConenection();
+             final PreparedStatement dropIfExistsTableCableDb = connection.prepareStatement("drop table if exists cables");
+             final PreparedStatement dropIfExistsTableCableCu = connection.prepareStatement("drop table if exists cables");
     }
 }
