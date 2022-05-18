0104d62bb93dd3cca092bf75cab861e89257211a
ivansevern <ivansevern@gmail.com>
2022-05-18 21:47:23 +0300

    add

--- src/main/java/ElectricalCalculatesForCables/CsvMapper.java
+++ src/main/java/ElectricalCalculatesForCables/CsvMapper.java
@@ -6,22 +6,23 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CsvMapper {
-    public static void main(String[] args) {
+    public static List<CableAlMany> getInfo() {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(CsvMapper.class.getResourceAsStream("/CableAlMany.csv")))) {
             CsvToBean<CableAlMany> csvToBean = new CsvToBeanBuilder(reader)
                     .withType(CableAlMany.class)
                     .withIgnoreLeadingWhiteSpace(true)
                     .build();
 
-            final List<CableAlMany> cable = csvToBean.stream().collect(Collectors.toList());
-            System.out.println(cable);
+            return csvToBean.stream().collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return Collections.emptyList();
     }
 }
 
